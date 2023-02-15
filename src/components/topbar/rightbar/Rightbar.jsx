ab675358aec99b6ff9a8689394cf7137cc818749
nisarga-nn <nisargan2000@gmail.com>
2023-02-16 00:29:00 +0530

    commit

--- src/components/topbar/rightbar/Rightbar.jsx
+++ src/components/topbar/rightbar/Rightbar.jsx
@@ -15,7 +15,58 @@ const Rightbar = () => {
         <h4 className="rightbarTitle">Online Friends</h4>
         <ul className="rightbarFriendList">
           <li className="rightbarFriend">
-            <div className="rightbarProfileImgCon">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="assets/person/nisargacv.jpg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">John Carter</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="assets/person/nisargacv.jpg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">John Carter</span>
+          </li> <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="assets/person/nisargacv.jpg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">John Carter</span>
+          </li> <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="assets/person/nisargacv.jpg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">John Carter</span>
+          </li> <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="assets/person/nisargacv.jpg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">John Carter</span>
+          </li> <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
               <img
                 src="assets/person/nisargacv.jpg"
                 alt=""
