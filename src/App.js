d75f4ff45e52792d38bb82dc827785d9a0ed6c3a
nisarga-nn <nisargan2000@gmail.com>
2023-02-16 18:01:09 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import Home from "./pages/home/Home";
 import Login from "./pages/home/login/Login";
 import Profile from "./pages/home/profile/Profile";
 import Signup from "./pages/home/signup/Signup";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Profile />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile/:username" element={<Profile />} />
+      </Routes>
+    </Router>
   );
 }
 
